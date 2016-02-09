6bb9b34fcad0468bfd955eff88d647fcd1020cf0
Benedikt Ritter <beneritter@gmail.com>
2016-02-09 13:34:06 +0100

    Add GNU Lesser General Public License v3 to the list of built-in licenses

--- src/main/scala/de/heikoseeberger/sbtheader/license/License.scala
+++ src/main/scala/de/heikoseeberger/sbtheader/license/License.scala
@@ -158,3 +158,23 @@ object GPLv3 extends License {
         |""".stripMargin
   }
 }
+
+object LGPLv3 extends License {
+  override def createLicenseText(yyyy: String, copyrightOwner: String) = {
+    s"""|Copyright (C) $yyyy  $copyrightOwner
+        |
+        |This program is free software: you can redistribute it and/or modify
+        |it under the terms of the GNU Lesser General Public License as published
+        |by the Free Software Foundation, either version 3 of the License, or
+        |(at your option) any later version.
+        |
+        |This program is distributed in the hope that it will be useful,
+        |but WITHOUT ANY WARRANTY; without even the implied warranty of
+        |MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        |GNU Lesser General Public License for more details.
+        |
+        |You should have received a copy of the GNU General Lesser Public License
+        |along with this program.  If not, see <http://www.gnu.org/licenses/>.
+        |""".stripMargin
+  }
+}
