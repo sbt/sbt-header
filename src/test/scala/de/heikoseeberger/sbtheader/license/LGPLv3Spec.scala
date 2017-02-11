9d7a11e21303692e5f43b16f3ed0de406703d47d
Benedikt Ritter <beneritter@gmail.com>
2017-02-11 17:01:21 +0100

    Add twirl comment style tests for all licenses

--- src/test/scala/de/heikoseeberger/sbtheader/license/LGPLv3Spec.scala
+++ src/test/scala/de/heikoseeberger/sbtheader/license/LGPLv3Spec.scala
@@ -97,6 +97,32 @@ class LGPLv3Spec extends WordSpec with Matchers {
       headerPattern shouldBe HeaderPattern.cppStyleLineComment
     }
 
+    "return the LGPLv3 license with Twirl style" in {
+      val (headerPattern, lgplv3) = LGPLv3("2015", "Heiko Seeberger", "@*")
+      val expected =
+        """|@****************************************************************************
+           | * Copyright (C) 2015  Heiko Seeberger                                      *
+           | *                                                                          *
+           | * This program is free software: you can redistribute it and/or modify     *
+           | * it under the terms of the GNU Lesser General Public License as published *
+           | * by the Free Software Foundation, either version 3 of the License, or     *
+           | * (at your option) any later version.                                      *
+           | *                                                                          *
+           | * This program is distributed in the hope that it will be useful,          *
+           | * but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+           | * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+           | * GNU Lesser General Public License for more details.                      *
+           | *                                                                          *
+           | * You should have received a copy of the GNU General Lesser Public License *
+           | * along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
+           | ****************************************************************************@
+           |
+           |""".stripMargin
+
+      lgplv3 shouldBe expected
+      headerPattern shouldBe HeaderPattern.twirlBlockComment
+    }
+
     "fail when unknown comment style prefix provided" in {
       intercept[IllegalArgumentException] { LGPLv3("2015", "Heiko Seeberger", "???") }
     }
