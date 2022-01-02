56410e800a476a05babf0cfacff95109c5b565dc
Scala Steward <me@scala-steward.org>
2022-01-02 02:44:41 +0100

    Reformat with scalafmt 3.3.1

--- bak/play-twirl/app/Filters.scala
+++ bak/play-twirl/app/Filters.scala
@@ -21,11 +21,10 @@ import filters.ExampleFilter
 @Singleton
 class Filters @Inject() (env: Environment, exampleFilter: ExampleFilter) extends HttpFilters {
 
-  override val filters = {
+  override val filters =
     // Use the example filter if we're running development mode. If
     // we're running in production or test mode then don't use any
     // filters at all.
     if (env.mode == Mode.Dev) Seq(exampleFilter) else Seq.empty
-  }
 
 }
